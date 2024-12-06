92711b35366d78652e6cdeb337392011f7f04e0b
mperwe <mperwe@gmail.com>
2024-12-06 23:47:41 +0100

    edit service pages

--- src/pages/consult.js
+++ src/pages/consult.js
@@ -14,39 +14,17 @@ const Software = () => {
   return (
     <div className="styles-container">
       {/* Services Section */}
-      <h2 className="styles-title"> 
-      <h2 className="page-title">Continuous IT Support & Services</h2>
+      <h2 className="styles-title">      <h2 className="page-title">Page under Construction....</h2>
       </h2>
       <div className="styles-grid">
         {/* Styles Cards */}
        
-        <a href="#" className="styles-card">
-          <img src={card2Img} alt="Data Cleaning & Preparation" className="styles-image" />
-          <h3 className="card-title"></h3>
-          <p className="card-description">
-          <b>24/7 Technical Support;</b> Offering round-the-clock assistance to resolve IT issues, ensuring minimal downtime and continuous system functionality.
-          </p>
-        </a>
         
-        <a href="#" className="styles-card">
-          <img src={card2Img} alt="Data Cleaning & Preparation" className="styles-image" />
-          <h3 className="card-title"></h3>
-          <p className="card-description">
-          <b>Proactive System Monitoring;</b>Constantly monitoring IT systems for potential issues, addressing problems before they impact operations, and ensuring optimal performance.
-          </p>
-        </a>
-
-        <a href="#" className="styles-card">
-          <img src={card3Img} alt="Descriptive Analytics" className="styles-image" />
-          <h3 className="card-title"></h3>
-          <p className="card-description">
-            <b>Scalable IT Solutions;</b> Offering flexible and scalable technology solutions that grow with your business needs, ensuring your IT infrastructure can adapt to changing demands.
-          </p>
-        </a>
+        
         
       </div>
 
-      
+           
     </div>
   );
 };
--- src/pages/training.js
+++ src/pages/training.js
@@ -43,6 +43,30 @@ const Software = () => {
           </p>
         </a>
             
+        <a href="#" className="styles-card">
+          <img src={card2Img} alt="Data Cleaning & Preparation" className="styles-image" />
+          <h3 className="card-title"></h3>
+          <p className="card-description">
+          <b>24/7 Technical Support;</b> Offering round-the-clock assistance to resolve IT issues, ensuring minimal downtime and continuous system functionality.
+          </p>
+        </a>
+        
+        <a href="#" className="styles-card">
+          <img src={card2Img} alt="Data Cleaning & Preparation" className="styles-image" />
+          <h3 className="card-title"></h3>
+          <p className="card-description">
+          <b>Proactive System Monitoring;</b>Constantly monitoring IT systems for potential issues, addressing problems before they impact operations, and ensuring optimal performance.
+          </p>
+        </a>
+
+        <a href="#" className="styles-card">
+          <img src={card3Img} alt="Descriptive Analytics" className="styles-image" />
+          <h3 className="card-title"></h3>
+          <p className="card-description">
+            <b>Scalable IT Solutions;</b> Offering flexible and scalable technology solutions that grow with your business needs, ensuring your IT infrastructure can adapt to changing demands.
+          </p>
+        </a>
+        
         
       </div>
 
